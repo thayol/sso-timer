03e93a7b43bbdc8965b1e59479379bb47d1550ca
Thayol <thayol@zovguran.net>
2023-12-02 01:11:26 +0100

    fix closing tags

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html lang="en-US">
   <head>
     <title>SSO Timer</title>
-    <meta charset="utf-8">
-    <meta name="author" content="Thayol" >
-    <meta name="description" content="A Championship timer for Star Stable Online" >
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="icon" type="image/png" sizes="32x32" href="src/favicon-32x32.png">
+    <meta charset="utf-8" />
+    <meta name="author" content="Thayol" />
+    <meta name="description" content="A Championship timer for Star Stable Online" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="icon" type="image/png" sizes="32x32" href="src/favicon-32x32.png" />
   </head>
   <body>
     <div class="background-image"></div>
@@ -14,7 +14,7 @@
     <div class="timer-container">
       <div class="timer-panel">
         <h2>Next Up</h2>
-        <h3 class="location-container">The <span id="location">...</span> Championship</h2>
+        <h3 class="location-container">The <span id="location">...</span> Championship</h3>
         <p class="championship">at <span id="time">00:00</span></p>
         <p class="remaining">Remaining: <span id="timer">00:00:00</span></p>
       </div>
